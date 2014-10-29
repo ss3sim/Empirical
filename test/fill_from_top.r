8edf518462d99a9751df842ed9a813d81a92e104
peterkuriyama <ptrkrym@uw.edu>
2014-10-28 18:54:47 -0700

    Cleaned up a little

--- test/fill_from_top.r
+++ test/fill_from_top.r
@@ -1,35 +1,4 @@
-
-# head(wtatage)
-
-
-# #Function to Fill in NaNs
-# minYear <- datfile$styr
-# maxYear <- datfile$endyr
-
-# mat <- as.data.frame(matrix(nrow = 3, ncol = 32))
-# names(mat) <- names(wtatage)
-
-# mat[1, ] <- wtatage[4, ]
-# mat[2, ] <- wtatage[4, ]
-# mat[3, ] <- wtatage[5, ]
-
-# mat[2, 1] <- 78
-# mat[3, 1] <- 80
-
-# mat[2, c(15, 28, 29, 30, 31, 32)] <- NaN
-# mat[3, c(17, 27, 28, 29, 30, 31, 32)] <- NaN
-
-# mat[2, c(7, 8, 14)] <- .858
-# mat[2, 9] <- .310
-
-# mat[3, c(9, 11, 13, 15)] <- .555
-# mat[3, 14] <- .666
-
-# fill_from_top(mat = mat, minYear = 1, maxYear = 100)
-
 ##Function that fills in matrix using first row of wtatage data
-
-
 fill_from_top <- function(mat, minYear, maxYear)
 {
   mat$yr <- abs(mat$yr)
@@ -66,4 +35,36 @@ fill_from_top <- function(mat, minYear, maxYear)
   }
   return(temp.df)
 }
+
+
+
+
+#Crap I used to check the function
+# head(wtatage)
+
+
+# #Function to Fill in NaNs
+# minYear <- datfile$styr
+# maxYear <- datfile$endyr
+
+# mat <- as.data.frame(matrix(nrow = 3, ncol = 32))
+# names(mat) <- names(wtatage)
+
+# mat[1, ] <- wtatage[4, ]
+# mat[2, ] <- wtatage[4, ]
+# mat[3, ] <- wtatage[5, ]
+
+# mat[2, 1] <- 78
+# mat[3, 1] <- 80
+
+# mat[2, c(15, 28, 29, 30, 31, 32)] <- NaN
+# mat[3, c(17, 27, 28, 29, 30, 31, 32)] <- NaN
+
+# mat[2, c(7, 8, 14)] <- .858
+# mat[2, 9] <- .310
+
+# mat[3, c(9, 11, 13, 15)] <- .555
+# mat[3, 14] <- .666
+
+# fill_from_top(mat = mat, minYear = 1, maxYear = 100)
 
