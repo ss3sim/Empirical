c241dc7c83613f5817d50be74bf57748c5ecc3f5
peterkuriyama <ptrkrym@uw.edu>
2015-02-20 11:35:24 -0800

    run analyses from here

---
+++ empirical_main.r
@@ -0,0 +1,17 @@
+#------------------------------------------------------------------------
+#Set Working Directory, load files
+setwd('/Users/peterkuriyama/School/Research/capam_growth/Empirical')
+
+
+library(devtools)
+load_all('ss3sim')
+load_all('ss3models')
+
+library(ss3sim)
+library(ss3models)
+
+#------------------------------------------------------------------------
+
+
+
+
