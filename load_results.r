575bcb1628a8e9919af85ddb640f0f6b63eb411c
peterkuriyama <ptrkrym@uw.edu>
2015-03-12 15:10:53 -0700

    load results

---
+++ load_results.r
@@ -0,0 +1,34 @@
+#load results into R
+results.sc <- read.csv('ss3sim_scalar.csv')
+results.ts <- read.csv('ss3sim_ts.csv')
+
+
+# emp.cases.df <- data.frame(species = species.vec,
+#                            samp.size = c('1000', '500',  
+#                                          '35, 75, 100', '35, 75, 100'))
+
+#Do calcluations on data frames
+results.sc$log_max_grad <- log(results.sc$max_grad)
+results.sc$converged <- ifelse(results.sc$max_grad<.1, "yes", "no")
+results.sc <- calculate_re(results.sc, add=TRUE)
+results.sc$runtime <- results.sc$RunTime
+
+#Drop fixed parameters(columns of 0)
+results.sc <- results.sc[,-which(apply(results.sc, 2, function(x) all(x==0)))]
+re.names <- names(results.sc)[grep('_re', names(results.sc))]
+
+results.sc.long <-
+    melt(results.sc, measure.vars=re.names,
+         id.vars= c("species", "replicate",
+         "log_max_grad", "params_on_bound_em", 'D', 'G', 'X', 'scenario'))
+
+growth.names <- re.names[grep("GP_", re.names)]
+results.sc.long.growth <- droplevels(subset(results.sc.long, variable %in% growth.names))
+results.sc.long.growth$variable <- gsub("_Fem_GP_1_re|_re", "", results.sc.long.growth$variable)
+selex.names <- re.names[grep("Sel_", re.names)]
+results.sc.long.selex <- droplevels(subset(results.sc.long, variable %in% selex.names))
+results.sc.long.selex$variable <- gsub("ery|ey|Size|_re", "", results.sc.long.selex$variable)
+results.sc.long.selex$variable <- gsub("_", ".", results.sc.long.selex$variable)
+management.names <- c("SSB_MSY_re", "depletion_re", "SSB_Unfished_re", "Catch_endyear_re")
+results.sc.long.management <- droplevels(subset(results.sc.long, variable %in% management.names))
+results.sc.long.management$variable <- gsub("_re", "", results.sc.long.management$variable)
